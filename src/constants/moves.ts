dd1a1d51b6312e9642c08ae55253928630189af0
Ziggy <zigzter@users.noreply.github.com>
2020-09-20 11:10:25 -0700

    React init

---
+++ src/constants/moves.ts
@@ -0,0 +1,7 @@
+export const attacks = ['jab', 'tilt', 'smash', 'special', 'air', 'dash', 'grab'];
+
+export const directions = ['forward', 'up', 'down', 'back', 'neutral'];
+
+export const moves = ['full hop', 'short hop', 'walk', 'run', 'dash'];
+
+export const modifiers = ['delayed', 'buffered', 'reverse'];
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <h1>Smash Trainer</h1>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
